df721d78973077b3fd93828d12a803a634f9e2f2
Fernando Moyano <nanomoyano@gmail.com>
2023-08-14 15:04:49 +0200

    Updated farmID

--- carbonplus_starter.R
+++ carbonplus_starter.R
@@ -29,7 +29,7 @@ settings <- list(
 # farmIds <- read_csv(file.path(sensitive_data_loc,"farmIds.csv"), show_col_types = FALSE)
 # farmId <- farmIds$farmId[2]
 # farmId <- "a9f9b719-b301-4a7d-a89c-824f73e2c966" # This is a test Id from user Suhas
-# farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab5c' # Troya
+# farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab51' # Troya
 # farmId <- '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b' # Valente
 # farmId <- '3f916c12-3a2c-4904-91cb-bb64e6fb0832' # Alves 1
 # farmId <- 'f67333e8-34a9-4030-93af-766f49d01310' # Alves 2
