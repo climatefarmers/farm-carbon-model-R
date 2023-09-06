209f798d4a6837e0afa59d51a1a2b2fbc5b3929f
Fernando Moyano <nanomoyano@gmail.com>
2023-09-06 10:50:43 +0200

    settings changes

--- carbonplus_starter.R
+++ carbonplus_starter.R
@@ -19,10 +19,10 @@ settings_testing <- list(
   get_grazing_estimates = FALSE,
   debug_mode = FALSE,
   save2mongoDB = FALSE,
-  copy_yearX_to_following_years_landUse = FALSE,
-  copy_yearX_to_following_years_livestock = FALSE,
-  yearX_landuse = NA,
-  yearX_livestock = NA,
+  copy_yearX_to_following_years_landUse = TRUE,
+  copy_yearX_to_following_years_livestock = TRUE,
+  yearX_landuse = 2,
+  yearX_livestock = 2,
   server = "dev",  # One of: "prod", "dev", "test"
   bare_bl_type = "reported", # One of: "envzone", "reported", "none". USE REPORTED, NOT ENVZONE!
   monitoring_run = TRUE, # (NOT YET IMPLEMENTED) TRUE/FALSE. If TRUE, not a prediction run and years after curr_monit_year will be excluded from the output
@@ -63,26 +63,26 @@ settings <- settings_testing
 # farmIds <- read_csv(file.path(sensitive_data_loc,"farmIds.csv"), show_col_types = FALSE)
 # farmId <- farmIds$farmId[2]
 # farmId <- "a9f9b719-b301-4a7d-a89c-824f73e2c966" # This is a test Id from user Suhas
-# farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab5c' # Troya
+farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab5c' # Troya
 # farmId <- '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b' # Valente
 # farmId <- '3f916c12-3a2c-4904-91cb-bb64e6fb0832' # Alves 1
 # farmId <- 'f67333e8-34a9-4030-93af-766f49d01310' # Alves 2
 # farmId <- '584b48dc-0e5d-4ecc-b7d4-9acf281faaba' # Alves 3
 # farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
 
-# out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
+out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
 
 
-farmIds <- c(
-  'edf5cce8-eee2-40a8-af32-520d2b93ab5c',
-  '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b',
-  '3f916c12-3a2c-4904-91cb-bb64e6fb0832',
-  'f67333e8-34a9-4030-93af-766f49d01310',
-  '584b48dc-0e5d-4ecc-b7d4-9acf281faaba',
-  'bb393d6d-f952-474e-a790-5486365d929b'
-)
-
-for (farmId in farmIds){
-  carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
-}
+# farmIds <- c(
+#   'edf5cce8-eee2-40a8-af32-520d2b93ab5c',
+#   '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b',
+#   '3f916c12-3a2c-4904-91cb-bb64e6fb0832',
+#   'f67333e8-34a9-4030-93af-766f49d01310',
+#   '584b48dc-0e5d-4ecc-b7d4-9acf281faaba',
+#   'bb393d6d-f952-474e-a790-5486365d929b'
+# )
+# 
+# for (farmId in farmIds){
+#   carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
+# }
 
--- carbonplus_starter_json.R
+++ carbonplus_starter_json.R
@@ -9,24 +9,23 @@ library(tidyverse)
 library(jsonlite)
 
 source("carbonplus_main.R")
-
 settings_testing <- list(
   n_runs = 2,
-  se_field_carbon_in = 0.0,
+  se_field_carbon_in = 0.001,
   get_grazing_estimates = TRUE,
-  debug_mode = TRUE,
+  debug_mode = FALSE,
   save2mongoDB = FALSE,
-  copy_yearX_to_following_years_landUse = FALSE,
-  copy_yearX_to_following_years_livestock = FALSE,
-  yearX_landuse = 1,
-  yearX_livestock = 1,
+  copy_yearX_to_following_years_landUse = TRUE,
+  copy_yearX_to_following_years_livestock = TRUE,
+  yearX_landuse = 2,
+  yearX_livestock = 2,
   server = "dev",  # One of: "prod", "dev", "test"
   bare_bl_type = "reported", # One of: "envzone", "reported", "none". USE REPORTED, NOT ENVZONE!
-  monitoring = TRUE, # Is this a monitoring run? If TRUE, no prediction adjustments are done until curr_monit_year. In particular, AMP projections using pasture_efficiency are deactivated.
-  curr_monit_year = 2  # Current monitoring year: what is the last year for which monitoring data has been provided
+  monitoring_run = TRUE, # (NOT YET IMPLEMENTED) TRUE/FALSE. If TRUE, not a prediction run and years after curr_monit_year will be excluded from the output
+  curr_monit_year = 2  # Current monitoring year: what is the last project year for which monitoring data has been provided
 )
 
 sensitive_data_loc <- "../sensitive-data"
 init_file <- fromJSON(file.path(sensitive_data_loc,"init_file.json"))
-jsonfile <- "../../data/farms/ManuealTroyaCantos_Dev20230707.json"
+jsonfile <- "../../data/pioneer_farms/farm_data/FranciscoAlves_4_Dev20230707.json"
 out <- carbonplus_main(init_file=init_file, settings=settings, JSONfile = jsonfile)
