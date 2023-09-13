a767cd72e4c22d44ba635c6b6b906d6b8f44cb0d
Fernando Moyano <nanomoyano@gmail.com>
2023-09-13 12:09:39 +0200

    Changed settings

--- carbonplus_starter.R
+++ carbonplus_starter.R
@@ -17,7 +17,7 @@ settings_testing <- list(
   n_runs = 2,
   se_field_carbon_in = 0.001,
   use_calculated_grazing = FALSE,
-  debug_mode = FALSE,
+  debug_mode = TRUE,
   save2mongoDB = FALSE,
   copy_yearX_to_following_years_landUse = TRUE,
   copy_yearX_to_following_years_livestock = TRUE,
@@ -26,7 +26,7 @@ settings_testing <- list(
   server = "dev",  # One of: "prod", "dev", "test"
   bare_bl_type = "reported", # One of: "envzone", "reported", "none". USE REPORTED, NOT ENVZONE!
   monitoring_run = TRUE, # (NOT YET IMPLEMENTED) TRUE/FALSE. If TRUE, not a prediction run and years after curr_monit_year will be excluded from the output
-  curr_monit_year = 10  # Current monitoring year: what is the last project year for which monitoring data has been provided
+  curr_monit_year = 2,  # Current monitoring year: what is the last project year for which monitoring data has been provided
 )
 settings <- settings_testing
 
@@ -64,11 +64,11 @@ settings <- settings_testing
 # farmId <- farmIds$farmId[2]
 # farmId <- "a9f9b719-b301-4a7d-a89c-824f73e2c966" # This is a test Id from user Suhas
 # farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab5c' # Troya
-farmId <- '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b' # Valente
+# farmId <- '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b' # Valente
 # farmId <- '3f916c12-3a2c-4904-91cb-bb64e6fb0832' # Alves 1
 # farmId <- 'f67333e8-34a9-4030-93af-766f49d01310' # Alves 2
 # farmId <- '584b48dc-0e5d-4ecc-b7d4-9acf281faaba' # Alves 3
-# farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
+farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
 
 out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
 
