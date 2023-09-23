d40d7fa0530ac7351861aa3024b4ba5d6dd6db91
Fernando Moyano <nanomoyano@gmail.com>
2023-09-23 23:02:20 +0200

    settings

--- carbonplus_starter.R
+++ carbonplus_starter.R
@@ -14,7 +14,7 @@ sensitive_data_loc <- "../sensitive-data"
 init_file <- fromJSON(file.path(sensitive_data_loc,"init_file.json"))
 
 settings_testing <- list(
-  n_runs = 2,
+  n_runs = 30,
   se_field_carbon_in = 0.1,
   se_inputs_nonfarm = 0.025,
   use_calculated_grazing = FALSE,
@@ -35,6 +35,7 @@ settings <- settings_testing
 # settings_predictions <- list(
 #   n_runs = 100,
 #   se_field_carbon_in = 0.1,
+#.  se_inputs_nonfarm = 0.025,
 #   use_calculated_grazing = TRUE,
 #   debug_mode = FALSE,
 #   save2mongoDB = TRUE,
@@ -45,10 +46,11 @@ settings <- settings_testing
 # )
 # settings <- settings_predictions
 
-## Uncomment block below for prediction production runs ------
+# # Uncomment block below for monitoring production runs ------
 # settings_monitoring <- list(
 #   n_runs = 100,
 #   se_field_carbon_in = 0.1,
+#   se_inputs_nonfarm = 0.025,
 #   use_calculated_grazing = TRUE,
 #   debug_mode = FALSE,
 #   save2mongoDB = TRUE,
@@ -69,21 +71,21 @@ settings <- settings_testing
 # farmId <- '3f916c12-3a2c-4904-91cb-bb64e6fb0832' # Alves 1
 # farmId <- 'f67333e8-34a9-4030-93af-766f49d01310' # Alves 2
 # farmId <- '584b48dc-0e5d-4ecc-b7d4-9acf281faaba' # Alves 3
-farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
-
-out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
-
-
-# farmIds <- c(
-  # 'edf5cce8-eee2-40a8-af32-520d2b93ab5c',
-  # '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b',
-#   '3f916c12-3a2c-4904-91cb-bb64e6fb0832',
-#   'f67333e8-34a9-4030-93af-766f49d01310',
-#   '584b48dc-0e5d-4ecc-b7d4-9acf281faaba',
-#   'bb393d6d-f952-474e-a790-5486365d929b'
-# )
+# farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
 # 
-# for (farmId in farmIds){
-#   carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
-# }
+# out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
+# 
+
+farmIds <- c(
+  'edf5cce8-eee2-40a8-af32-520d2b93ab5c',
+  '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b',
+  '3f916c12-3a2c-4904-91cb-bb64e6fb0832',
+  'f67333e8-34a9-4030-93af-766f49d01310',
+  '584b48dc-0e5d-4ecc-b7d4-9acf281faaba',
+  'bb393d6d-f952-474e-a790-5486365d929b'
+)
+
+for (farmId in farmIds){
+  carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
+}
 
