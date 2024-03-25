b83a9d881564adc853908f1827f3b5f415f101b3
Fernando Moyano <nanomoyano@gmail.com>
2024-03-25 15:23:11 +0100

    Cleaned up starter file.

--- carbonplus_starter.R
+++ carbonplus_starter.R
@@ -58,26 +58,11 @@ settings <- settings_testing
 
 ## Run with mongoDB data -------------------------------------------------------
 
-# farmIds <- read_csv(file.path(sensitive_data_loc,"farmIds.csv"), show_col_types = FALSE)
-# farmId <- farmIds$farmId[2]
-# farmId <- "a9f9b719-b301-4a7d-a89c-824f73e2c966" # This is a test Id from user Suhas
-# farmId <- 'edf5cce8-eee2-40a8-af32-520d2b93ab5c' # Troya
-# farmId <- '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b' # Valente
-# farmId <- '3f916c12-3a2c-4904-91cb-bb64e6fb0832' # Alves 1
-# farmId <- 'f67333e8-34a9-4030-93af-766f49d01310' # Alves 2
-# farmId <- '584b48dc-0e5d-4ecc-b7d4-9acf281faaba' # Alves 3
-# farmId <- 'bb393d6d-f952-474e-a790-5486365d929b' # Alves 4
-# 
+# farmId <- 
 # out <- carbonplus_main(init_file=init_file, settings=settings, farmId=farmId)
 
-
 # farmIds <- c(
-  # 'edf5cce8-eee2-40a8-af32-520d2b93ab5c',
-  # '7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b',
-#   '3f916c12-3a2c-4904-91cb-bb64e6fb0832',
-#   'f67333e8-34a9-4030-93af-766f49d01310',
-#   '584b48dc-0e5d-4ecc-b7d4-9acf281faaba',
-#   'bb393d6d-f952-474e-a790-5486365d929b'
+# list of farm_IDs
 # )
 # 
 # for (farmId in farmIds){
@@ -87,26 +72,14 @@ settings <- settings_testing
 
 ## Run with json file ----------------------------------------------------------
 
-# jsonfile <- "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Alves4_bb393d6d-f952-474e-a790-5486365d929b.json"
-# jsonfile <- "../../data/pioneer_farms/inputs/2023-10-10_minor_corrections/Troya_edf5cce8-eee2-40a8-af32-520d2b93ab5c.json"
-# jsonfile <- "../../data/pioneer_farms/inputs/2023-10-10_minor_corrections/Valente_7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b.json"
-# out <- carbonplus_main(init_file=init_file, settings=settings, JSONfile = jsonfile)
+jsonfile = here("example-farm-data","example-farm-monitoringData.json")
+out <- carbonplus_main(init_file=init_file, settings=settings, JSONfile = jsonfile)
 
 # json_files <- c(
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Alves1_3f916c12-3a2c-4904-91cb-bb64e6fb0832.json",
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Alves2_f67333e8-34a9-4030-93af-766f49d01310.json",
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Alves3_584b48dc-0e5d-4ecc-b7d4-9acf281faaba.json",
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Alves4_bb393d6d-f952-474e-a790-5486365d929b.json",
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Troya_edf5cce8-eee2-40a8-af32-520d2b93ab5c.json",
-#   # "../../data/pioneer_farms/farm_data/2023-09-14_corrected_before_animal_numbers_update/Valente_7fe9ced2-73b8-45aa-b6a2-a9ede144ca1b.json"
+# list of json files
 # )
 # 
 # for (jsonfile in json_files){
 #   carbonplus_main(init_file=init_file, settings=settings, JSONfile = jsonfile)
 # }
 
-## Run with example json
-# jsonfile_json_format = here("example-farm-data","json_format.json")
-jsonfile_example_farm = here("example-farm-data","example-farm-monitoringData.json")
-
-#out <- carbonplus_main(init_file=init_file, settings=settings, JSONfile = jsonfile)
